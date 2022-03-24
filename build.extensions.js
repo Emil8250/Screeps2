a5e9ff9bc304f6cc09f0a25819152db61dd2e0a7
eso <emil8250@ymail.com>
2022-03-24 21:21:33 +0100

    Added correct flagpos for spawning extensions

--- build.extensions.js
+++ build.extensions.js
@@ -2,14 +2,9 @@ var buildExtenions = {
 
     run: function (room) {
         var flag = Game.flags.extensions;
-        var size = 5;
-        var start = 2
-        if (flag === undefined)
-        {
-            return;
-        }
+        var size = 8;
         var flagPos = flag.pos;
-        var cornerPos = new RoomPosition(flagPos.x - start, flagPos.y - start, room);
+        var cornerPos = new RoomPosition(flagPos.x, flagPos.y, room);
         for (let i = 0; i < size; i++) {
             for (let j = 0; j < size; j++) {
                 var isBothEven = i % 2 === 0 && j % 2 === 0;
