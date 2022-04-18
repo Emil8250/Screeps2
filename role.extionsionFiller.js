b9d7bb4314ed49ee7610e3d958a050a6a68599dc
Emil Olesen <emil8250@ymail.com>
2022-04-18 22:26:04 +0200

    Handle multiple extension fillers

--- role.extionsionFiller.js
+++ role.extionsionFiller.js
@@ -2,12 +2,14 @@ var misc = require('misc.methods');
 var extensionFiller = {
     /** @param {Creep} creep **/
     run: function (creep) {
+        var fillers = _.filter(Game.creeps, (creep) => creep.memory.role == 'extensionFiller');
         var extensions = creep.room.find(FIND_STRUCTURES, {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
                     structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
             }
         });
+        var takenExtensions = [];
         if (creep.store.getFreeCapacity() == 0) {
             creep.memory.filling = true;
         }
@@ -15,9 +17,19 @@ var extensionFiller = {
             creep.memory.filling = false;
         }
         if (creep.memory.filling) {
-            if (creep.transfer(extensions[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(extensions[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            for (let i = 0; i < fillers.length; i++)
+            {
+                if(i == 0){
+                    if (creep.transfer(extensions[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(extensions[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }   
+                }
+                else{
+                    if (creep.transfer(extensions[extensions.length - 1], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(extensions[extensions.length - 1], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
             }
+        }
         } else {
             misc.FetchEnergy(creep);
         }
